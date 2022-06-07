54d048c07f0642d682efdf1afff21a320c8aa04d
Daniele Spadoni <d.spadoni@infordata.net>
2022-06-07 20:39:30 +0200

    Lesson 16 - Test JUnit Flux

--- reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
+++ reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
@@ -2,6 +2,23 @@ package com.learnreactiveprogramming.service;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Test;
+
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
 class FluxAndMonoGeneratorServiceTest {
 
+	FluxAndMonoGeneratorService fluxAndMonoGeneratorService = new FluxAndMonoGeneratorService();
+	
+	@Test
+	void namesFluxTest() {
+		
+		Flux<String> names = fluxAndMonoGeneratorService.namesFlux();
+		
+		StepVerifier.create(names)
+			.expectNext("alex", "ben", "chloe")
+			.verifyComplete();
+	}
+	
 }
