6cc974ce9ba5af04144721385df2e4c82f5278a0
Daniele Spadoni <d.spadoni@infordata.net>
2022-06-07 14:44:39 +0200

    Lesson 14 - Esempio di Mono

--- reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
+++ reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
@@ -3,18 +3,39 @@ package com.learnreactiveprogramming.service;
 import java.util.List;
 
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 public class FluxAndMonoGeneratorService {
 
+	/**
+	 * Esempio di Flux di stringhe
+	 * Un flux puo' pubblicare da 0 a n elementi
+	 * @return
+	 */
 	public Flux<String> namesFlux(){
 		//Simula una chiamata a db o servizio remoto
 		return Flux.fromIterable(List.of("alex", "ben", "chloe")); 
 	}
 	
+	/**
+	 * Esempio di Mono applicato su stringa
+	 * Un mono puo' pubblicare da 0 a 1 elemento
+	 * @return
+	 */
+	public Mono<String> nameMono() {
+		return Mono.just("alice");
+	}
+	
    public static void main(String[] args) {
 	   FluxAndMonoGeneratorService fluxAndMonoGeneratorService = new FluxAndMonoGeneratorService();
+	   
+	   //Richiamo il flux e eseguo il subscribe di ogni elemento del flux
 	   fluxAndMonoGeneratorService.namesFlux()
 	   					.subscribe(name -> System.out.println("Name is "+name));
+	   
+	   //Richiamo il mono ed eseguo il subscribe del singolo elemento
+	   fluxAndMonoGeneratorService.nameMono()
+			.subscribe(name -> System.out.println("Mono Name is "+name));
    }
 
 }
